57a2509e02475836562381a613609ef5869937ac
HoVanThang <hothang2262004@gmail.com>
2023-10-09 00:58:06 +0700

    update

--- java.js
+++ java.js
@@ -29,6 +29,5 @@ window.onscroll = () => {
     cartItem.classList.remove('active');
 }
 function redirectToBuyPage() {
-    // Chuyển hướng sang trang mua hàng (ví dụ: 'buy.html')
-    window.location.href = 'http://127.0.0.1:5500/main1.html';
+    window.location.href = 'main1.html';
   }
